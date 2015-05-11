9f6f70c86d68a0434a01dbb7b845e59cba35b3ee
Alejandro Iglesias Ruiz <alejandro.iglesias.ruiz@alumnos.upm.es>
2015-05-11 20:08:52 +0200

    Autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+var sessionController = require('../controllers/session_controller');
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
@@ -18,11 +21,15 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/search', quizController.search);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
-
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
-
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+//Definicion de rutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
 //Ruta de athor
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Autores', errors: [] });

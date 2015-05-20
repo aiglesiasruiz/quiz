739a7a5dc4e274e71657e3690cdeedcb99b67519
Alejandro Iglesias Ruiz <alejandro.iglesias.ruiz@alumnos.upm.es>
2015-05-20 13:22:32 +0200

    Mis preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -62,8 +62,13 @@ exports.new = function(req, res){
 
 //GET /quizes
 exports.index = function(req, res){
+  var options = {};
+  if(req.user){
+    options.where = {UserId: req.user.id}
+  }
+  
   if(req.query.busqueda===undefined){
-    models.Quiz.findAll().then(
+    models.Quiz.findAll(options).then(
       function(quizes) {
       res.render('quizes/index.ejs', {quizes: quizes, errors: []});
      }).catch (function (error) { next(error)});
